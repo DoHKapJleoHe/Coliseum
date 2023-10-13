91be365dcf77ef47f225ab323ee108ce42f6000d
DoHKapJleoHe <91648774+DoHKapJleoHe@users.noreply.github.com>
2023-10-13 16:55:37 +0700

    made writing to sqlite

---
+++ DbLib/Database.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DbLib;
+
+public class DeckDbContext : DbContext
+{
+    public DbSet<UserEntity> Users { get; set; }
+    public DbSet<DeckEntity> Decks { get; set; }
+
+    public DeckDbContext()
+    {
+        Database.EnsureCreated();
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite($"Data Source=decks.db");
+    }
+}
