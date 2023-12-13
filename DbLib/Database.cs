0e3f6bcdf26bfdc3a91e479165d86870051b3746
Eduard Vartazaryan <91648774+DoHKapJleoHe@users.noreply.github.com>
2023-12-13 16:45:57 +0700

    Delete DbLib directory

--- DbLib/Database.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace DbLib;
-
-public class DeckDbContext : DbContext
-{
-    public DbSet<DeckEntity> Decks { get; set; }
-    public DeckDbContext(DbContextOptions<DeckDbContext> options) : base(options) { }
-}
